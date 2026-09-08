41d119c0cb1c0a26a0d15e0031e718047e39a074
mahdimojdehpoor <mahdimojdehpoor@gmail.com>
2026-09-08 22:38:20 +0330

    Remove Compose plugin and add compose options
    Removed the Compose plugin and added compose options with a specific Kotlin compiler extension version.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.plugin.compose")
     id("org.jetbrains.kotlin.plugin.serialization")
 }
 
@@ -29,6 +28,10 @@ android {
     buildFeatures {
         compose = true
         buildConfig = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.14"
     }
 
     buildTypes {
