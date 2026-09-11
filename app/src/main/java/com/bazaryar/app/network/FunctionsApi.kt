85f052c3cf0447626025382933781e8e16c10fcd
mahdimojdehpoor <mahdimojdehpoor@gmail.com>
2026-09-11 18:29:34 +0330

    Update FunctionsApi.kt

--- app/src/main/java/com/bazaryar/app/network/FunctionsApi.kt
+++ app/src/main/java/com/bazaryar/app/network/FunctionsApi.kt
@@ -1,9 +1,11 @@
 package com.bazaryar.app.network
 
+import com.bazaryar.app.model.DeleteAccountRequest
 import com.bazaryar.app.model.SimpleResponse
+import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface FunctionsApi {
     @POST("delete-account")
-    suspend fun deleteAccount(): SimpleResponse
+    suspend fun deleteAccount(@Body body: DeleteAccountRequest = DeleteAccountRequest()): SimpleResponse
 }
